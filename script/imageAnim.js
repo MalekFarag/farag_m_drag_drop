cd981ba7ae0521821e445bd608196690b35ea458
MalekFarag <43182938+MalekFarag@users.noreply.github.com>
2019-02-04 13:43:07 -0500

    holy shit it works

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -1,53 +1,91 @@
 (() => {
-	console.log('js is ok')
+	console.log('goteem');
+	
 	// set up the puzzle pieces and boards
-	// 
-	//document.querySelector('h1').textContent = 'bonjour' (this changes the text)
+
+	//need a reference to each piece that we want to create
+	const thePieces = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
 	
-	//function changeHeadline() {
-	//	document.querySelector('h1').textContent = 'bonjour';
-	//	document.querySelector('p').textContent = 'oh no you dingus!yall just got CLICKED';
-	//}
-		
-
-	//const theButton = document.querySelector('#buttonHolder img', changeHeadline) //this targets 'theButton' ID and links it to the function
-
-
-	//theButton.addEventListener('click', changeHeadline); //plays function fter 'loading page' or after event listener happens
-
-
-	const thePieces = ['topLeft', 'topRight', 'bottomLeft', 'bottomRight'];
-	//'pieces' = 4 images of large picture
-	
-
+	// get a reference to the drag side
 	let piecesBoard = document.querySelector('.puzzle-pieces');
-	let puzzleBoard = document.querySelector('.puzzleBoard');
+	let puzzleBoard = document.querySelector('.puzzle-board');
 
-	let puzzleSelectors = document.querySelectorAll('#buttonHolder img')
+	//get a reference to the buttons at the bottom so we can change the puzzle
+	let puzzleSelectors = document.querySelectorAll('#buttonHolder img');
 
-	function createPuzzlePiece(pictureIndex) {
-		//add images here!
-		//debugger;
-		//
-		thePieces.forEach((piece, index) => {//******fix this
-			let newPuzzlePiece = `<img id="piece${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="puzzle piece">`;
+
+	// DROP ZONES******
+	let dropZones = document.querySelectorAll('.drop-zone');
+
+
+
+	// functions go in the middle
+	function createPuzzlePieces(pictureIndex) {
+		// generate images here -> need to make 4 (top left, top right, bottom left and bottom right)
+		// debugger;	
+		//loop through the images refs and generate one for each 
+		thePieces.forEach((piece, index)=> {
+			let newPuzzlePiece = `<img id="piece${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="puzzle piece" draggable>`;
 
 			piecesBoard.innerHTML += newPuzzlePiece;
-			//debugger;
 		});
-
+		initDrag();
 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		//********drag + drop funtionality*******
+		//
+	function initDrag() {
+		piecesBoard.querySelectorAll('img').forEach(img =>{
+			img.addEventListener("dragstart", function(e){
+				console.log('dragging...');
+				e.dataTransfer.setData('text/plain', this.id);
+			});
+		});
+	}
+
+	//coding the drop
+	//
+	dropZones.forEach(zone => {
+		zone.addEventListener('dragover', function(e){
+			e.preventDefault(); //prevents default (reload page (in this case))
+			console.log('dragged')
+		});
+		zone.addEventListener('drop', function(e){
+			e.preventDefault();
+			console.log('dropped')
+
+			let piece = e.dataTransfer.getData('text/plain');
+			e.target.appendChild(document.querySelector(`#${piece}`));
+		});
+	});
 
 	function resetPuzzlePieces() {
-		//reset puzzle
-		//debugger;
-		piecesBoar.innerHTML = "";
+		// change the current puzzle, regenerate the pieces
+		// debugger;
+		// clean out the puzzle pieces div
 
-		createPuzzlePiece(this.dataset.puzzleref)
+		piecesBoard.innerHTML = "";
+		// generate new pieces
+		createPuzzlePieces(this.dataset.puzzleref);
 	}
 
-	//event handling goes here
-	puzzleSelectors.forEach(button => button.addEventListener('click', resetPuzzlePieces));
+	// event handling goes here
+	puzzleSelectors.forEach(button => button.addEventListener("click", resetPuzzlePieces));
 
-	createPuzzlePiece(0);
+	// call this function to set up / generate the pieces on load
+	createPuzzlePieces(0);
+
 })();
